fae5ae2cff1fb306a2ef17d481d8b9354fca2648
saanandsthavara <saanand@protonmail.com>
2022-09-10 12:50:28 +0530

    added cartContext component for context API and custom hooks

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,5 +1,7 @@
+import clsx from 'clsx';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import styles from '../styles/StoreItems.module.css';
 
 type StoreItemsProps = {
   id: number;
@@ -9,6 +11,8 @@ type StoreItemsProps = {
 };
 
 export const StoreItems = ({ id, name, price, imageUrl }: StoreItemsProps) => {
+  // declaring hard-coded value as of now
+  const quantity = 0;
   return (
     <>
       <Card className='h-100'>
@@ -23,7 +27,42 @@ export const StoreItems = ({ id, name, price, imageUrl }: StoreItemsProps) => {
             <span className='fs-2'>{name}</span>
             <span className='ms-2 text-muted'>{price}</span>
           </Card.Title>
-          <div className='mt-auto'></div>
+          <div className='mt-auto'>
+            {quantity === 0 ? (
+              <Button
+                variant='primary'
+                className='w-100'>
+                + Add to Cart
+              </Button>
+            ) : (
+              <div
+                className={clsx(
+                  `${styles.itemsButton}`,
+                  'd-flex',
+                  'align-items-center',
+                  'flex-column'
+                )}>
+                <div
+                  className={clsx(
+                    `${styles.itemsButton}`,
+                    'd-flex',
+                    'align-items-center',
+                    'justify-content-center'
+                  )}>
+                  <Button>-</Button>
+                  <div>
+                    <span className='fs-3'>{quantity}</span> in Cart
+                  </div>
+                  <Button>+</Button>
+                </div>
+                <Button
+                  variant='danger'
+                  size='sm'>
+                  Remove Items
+                </Button>
+              </div>
+            )}
+          </div>
         </Card.Body>
       </Card>
     </>
---
+++ src/context/CartContext.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// This component is going to be a react context api where we will be utilising the context api which is used for overcoming the problem of prop drillings in react
+// we will also be utilising the custom hooks in this application
+export const CartContext = () => {
+  return <div>CartContext</div>;
+};
