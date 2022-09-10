2061bdb292951780c2894b92be851f2d4d82e2ca
saanandsthavara <saanand@protonmail.com>
2022-09-11 00:43:22 +0530

    implemented context API and common logic for the components

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { createContext, ReactNode, useContext } from 'react';
 // This component is going to be a react context api where we will be utilising the context api which is used for overcoming the problem of prop drillings in react
 // we will also be utilising the custom hooks in this application
-export const CartContext = () => {
-  return <div>CartContext</div>;
+type ShoppingCartProviderProps = {
+  children: ReactNode;
 };
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => number;
+};
+
+const CartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(CartContext);
+}
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
+}
